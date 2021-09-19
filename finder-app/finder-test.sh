faa3fde607d4b19b0217d3999b3a95e6693241b5
Vishnu Dodballapur <vido2373@colorado.edu>
2021-09-18 18:48:41 -0600

    Another small change

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -47,7 +47,7 @@ do
 	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 echo ${OUTPUTSTRING} > /tmp/assignment-4-result.txt
 
 set +e
